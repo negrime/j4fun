e54360f2ad220cf1f236d279cb12c7aa15838cc4
vladislav <kolupaevvladislav@mail.ru>
2020-08-25 20:58:42 +0300

    test push

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict"
+
+const APP_ID = "7577780";
+const AUTH_LINK = "https://oauth.vk.com/authorize?client_id=" + APP_ID + "&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=offline&wall&groups&response_type=token&v=5.52";
+const ACCESS_TOKEN = "91a8c8c1919e2518d118357436d0d129eb7fc1db51a2d7a1212780f9016934b9d8d4b77d001894afb552b";
+const SERVICE_KEY = "8e9d9bae8e9d9bae8e9d9bae4e8eee3b1a88e9d8e9d9baed1d8a8ffd8000c37c78c8c59";
+console.log("https://api.vk.com/method/wall.createComment?owner_id=-143393434&post_id=629&message=test&access_token=" + ACCESS_TOKEN + "&v=5.52");
+
+console.log("https://api.vk.com/method/wall.get?owner_id=-143393434&count=1&message=test&access_token=" + ACCESS_TOKEN + "&v=5.52");
+
+setInterval(kek, 1000);
+
+function kek()
+{
+    $.ajax({
+        url: "https://api.vk.com/method/wall.createComment?owner_id=-143393434&post_id=629&message=test&access_token=" + ACCESS_TOKEN + "&v=5.52",
+        type: 'GET',
+        dataType: 'jsonp',
+        success: function(data){
+            console.log(data.response);
+            //   data.response.items.forEach(el => console.log("https://vk.com/" +el.screen_name));
+        }
+    })
+}
+
+
